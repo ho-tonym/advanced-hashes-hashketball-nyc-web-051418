26f1c10073939b58359aafcb4750045fa106a455
Tony Ho <ho.tonym@gmail.com>
2018-05-11 13:30:33 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -212,8 +212,6 @@ def big_shoe_rebounds
   biggest_shoe = 0
   num_rebounds = 0
 
-  game_data = game_hash.values
-
     game_data[:players].each do |player, game_stats|
       if game_stats[:shoe] > biggest_shoe
         biggest_shoe = game_stats[:shoe]
